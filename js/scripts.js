afc69051f853224f7ead9198454cf27e2771d62c
Alex Lantz <aclantz321@gmail.com>
2023-10-28 11:20:57 -0400

    add for loop, printing object array to dom

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ let pokemonList = [
     {name: 'Squirtle', type: 'water', height: 0.5}, 
     {name: 'Charmander', type: 'fire', height: 0.6}
 ]
+
+//Loop to print pokemonList object names and heights to the DOM
+for (i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 0.7) {
+        document.write(pokemonList[i].name + " " + pokemonList[i].height + "<br>");//used a string to space between printed object keys, and to create a line break.
+    }
+    else {
+        document.write(pokemonList[i].name + " " + pokemonList[i].height + " " + "Wow, that's big!<br>"); // highlight largest pokemon
+    }
+}
+
+
