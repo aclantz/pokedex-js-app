4406a886f2ffb60ac3c487416d0bf50e5fdd5f39
Alex Lantz <aclantz321@gmail.com>
2023-11-04 09:20:42 -0400

    add IIFE, update forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
-let pokemonList = [
-    {name: 'Bulbasaur', type: ['grass', 'poison'], height: 2.4}, 
-    {name: 'Squirtle', type: 'water', height: 1.8}, 
-    {name: 'Charmander', type: 'fire', height: 2}
-]
 
-pokemonList.forEach(function(pokemon) {
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', 
+        type: ['grass', 'poison'], 
+        height: 2.4
+        }, 
+        {name: 'Squirtle', 
+        type: 'water', 
+        height: 1.8
+        }, 
+        {name: 'Charmander', 
+        type: 'fire', 
+        height: 2
+        }
+    ]
+    function getAll() {
+        return pokemonList
+    }
+    function add() {
+        pokemonList.push
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+})()
+
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + ' is a ' + pokemon.type + ' type pokemon, who is ' + pokemon.height + ' feet tall. <br>' )
 })
 
